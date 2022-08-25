0c32de07e968bfe482afa2f71b02324c72d108b0
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-26 01:24:36 +0530

    26/08/2022

--- LinkedListTraversal.c
+++ LinkedListTraversal.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node* Next ;
+};
+int main(){
+    struct Node* start = (struct Node*)malloc(sizeof(struct Node));
+    int num = 0;
+    do{
+        printf("Enter the Data : ");
+        scanf("%d",&num);
+        if(num != 0){
+            struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+            start->Next = node;
+            node->data = num ; 
+            node->Next = NULL;
+        }
+    }while(num != 0);
+    return 0;
+}
