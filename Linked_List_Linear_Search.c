342499ed220445b2fefc4cee8799339d0763c6b3
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-26 23:15:20 +0530

    26/08/2022

--- Linked_List_Linear_Search.c
+++ Linked_List_Linear_Search.c
@@ -5,12 +5,44 @@ struct Node{
     struct Node* next;
 };
 void getData(struct Node*);
+void searchElement(struct Node*);
 int main(){
-    struct Node *head, *new;
-    new = (struct Node*)malloc(sizeof(struct Node));
-    head = ptr = new;
-
-
-
+    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
+    getData(head);
+    searchElement(head);
     return 0 ;
+}
+void getData(struct Node* ptr){
+    int count = 0;
+    char ch ;
+    do{
+        printf("Enter the value of Node : ");
+        if(!count){
+            scanf("%d",&ptr->data);
+            count++;
+        }
+        else{
+            struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+            scanf("%d",&new->data);
+            ptr->next = new ;
+            ptr = new;
+        }
+        printf("Do you wish to Continue (y/n) : ");
+        getchar();
+        scanf("%c",&ch);
+    }while(ch == 'y');
+}
+void searchElement(struct Node* ptr){
+    int search;
+    printf("Enter the Element to be Searched : ");
+    scanf("%d",&search);
+    while(ptr != NULL){
+        if(search == ptr->data){
+            printf("Element Found !!\n");
+            break;
+        }
+        ptr = ptr->next;
+    }
+    if(ptr == NULL)
+        printf("No Element Found !!\n");
 }
