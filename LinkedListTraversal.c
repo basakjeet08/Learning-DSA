1a1c50352920c42913af21c20b63e4a170e40aac
Anirban Basak <jeetbasak2002@gmail.com>
2022-08-26 22:52:02 +0530

    26/08/2022

--- LinkedListTraversal.c
+++ LinkedListTraversal.c
@@ -2,20 +2,42 @@
 #include <stdlib.h>
 struct Node{
     int data;
-    struct Node* Next ;
+    struct Node* next;
 };
+void getData(struct Node* );
+void showData(struct Node*);
 int main(){
-    struct Node* start = (struct Node*)malloc(sizeof(struct Node));
-    int num = 0;
+    struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+    struct Node* head, *ptr;
+    head = ptr = new ;
+    getData(head);
+    showData(head);
+    return 0 ;
+}
+void getData(struct Node* ptr){
+    char ch;
+    int count = 0;
     do{
-        printf("Enter the Data : ");
-        scanf("%d",&num);
-        if(num != 0){
-            struct Node* node = (struct Node*)malloc(sizeof(struct Node));
-            start->Next = node;
-            node->data = num ; 
-            node->Next = NULL;
+        printf("Enter the Value of Node : ");
+        if(!count){
+            scanf("%d",&ptr->data);
+            count++;
         }
-    }while(num != 0);
-    return 0;
+        else{
+            struct Node* new = (struct Node*)malloc(sizeof(struct Node));
+            scanf("%d",&new->data);
+            ptr->next = new;
+            ptr = new;
+        }
+        printf("Do u wish to Continue(y/n) : ");
+        getchar();
+        scanf("%c",&ch);
+    }while(ch == 'y');
+}
+void showData(struct Node* ptr){
+    printf("The Values of Linked List : \n");
+    while(ptr != NULL){
+        printf("%d\n",ptr->data);
+        ptr = ptr->next;
+    }
 }
